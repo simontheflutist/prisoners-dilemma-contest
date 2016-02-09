0dbd9bed7b406d75ad758d4f60613ca7591cea5c
Simon Kuang <simonkuangflutist@gmail.com>
2016-02-09 14:53:00 -0800

    the play() method looks like it works for now.

--- src/prisonersdilemmacontest/TwoPlayerGame.java
+++ src/prisonersdilemmacontest/TwoPlayerGame.java
@@ -16,4 +16,37 @@ public final class TwoPlayerGame {
         this.player1 = player1;
         this.player2 = player2;
     }
+
+    /**
+     * Play a game of the Prisoner's Dilemma.
+     *
+     * @return the score account where the first number is player1's score and
+     * the second number is player2's score
+     */
+    public double[] play() {
+        boolean player1first = Math.random() < 0.5;
+        PrisonerPlayer playerA = player1first ? player1 : player2;
+        PrisonerPlayer playerB = player1first ? player2 : player1;
+
+        final double[] tally = new double[]{0, 0};
+
+        int roundCount = 0;
+        do {
+            boolean currentMoveA = playerA.nextMove();
+            playerB.processOpponentMove(currentMoveA);
+
+            // playerB can have an advantage, for example, if he is move-copying
+            boolean currentMoveB = playerB.nextMove();
+            playerA.processOpponentMove(currentMoveB);
+
+            double[] d_score = ROUND_OUTCOMES[currentMoveA ? 1 : 0][currentMoveB ? 1 : 0];
+
+            tally[0] += d_score[0];
+            tally[1] += d_score[1];
+
+            roundCount++;
+        } while (roundCount < NUMBER_OF_ROUNDS);
+
+        return player1first ? tally : new double[]{tally[1], tally[0]};
+    }
 }
