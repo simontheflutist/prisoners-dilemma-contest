3610e928c6777a9a327b19fe1d9848483b47b88f
Simon Kuang <simonkuangflutist@gmail.com>
2016-02-09 15:55:32 -0800

    RRT and test code in main

--- src/prisonersdilemmacontest/PrisonersDilemmaContest.java
+++ src/prisonersdilemmacontest/PrisonersDilemmaContest.java
@@ -1,12 +1,16 @@
 package prisonersdilemmacontest;
 
-import java.util.Arrays;
+import prisonersdilemmacontest.prisoners.CopyMovePrisonerMean;
+import prisonersdilemmacontest.prisoners.CopyMovePrisonerNice;
+import prisonersdilemmacontest.prisoners.MeanPrisoner;
 import prisonersdilemmacontest.prisoners.RandomPrisoner;
 
 public class PrisonersDilemmaContest {
 
     public static void main(String[] args) {
-        TwoPlayerGame game = new TwoPlayerGame(new RandomPrisoner(), new RandomPrisoner());
-        System.out.println(Arrays.toString(game.play()));
+//        TwoPlayerGame game = new TwoPlayerGame(new RandomPrisoner(), new CopyMovePrisonerMean());
+//        System.out.println(Arrays.toString(game.play()));
+        RoundRobinTournament rrt = new RoundRobinTournament(new RandomPrisoner(), new MeanPrisoner(), new CopyMovePrisonerMean(), new CopyMovePrisonerNice());
+        rrt.runTournament();
     }
 }
--- src/prisonersdilemmacontest/RoundRobinTournament.java
+++ src/prisonersdilemmacontest/RoundRobinTournament.java
@@ -6,6 +6,7 @@
 package prisonersdilemmacontest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,7 +17,7 @@ import java.util.List;
  */
 public class RoundRobinTournament {
 
-    class ScoredPrisoner {
+    protected static class ScoredPrisoner {
 
         private final PrisonerPlayer prisoner;
         private double score;
@@ -24,28 +25,70 @@ public class RoundRobinTournament {
         public ScoredPrisoner(PrisonerPlayer prisoner) {
             this.prisoner = prisoner;
             this.score = 0;
-
         }
 
-        public boolean nextMove() {
-            return prisoner.nextMove();
+        public void addToScore(double dscore) {
+            this.score += dscore;
         }
 
-        public void processOpponentMove(boolean oppMove) {
-            prisoner.processOpponentMove(oppMove);
+        public double getScore() {
+            return this.score;
+        }
+
+        public PrisonerPlayer getPrisoner() {
+            return this.prisoner;
         }
 
     }
 
-    public static int GAMES_PER_SET = 100;
+    public static int GAMES_PER_SET = 10;
 
     private final List<ScoredPrisoner> prisoners;
 
-    public RoundRobinTournament(List<PrisonerPlayer> prisoners) {
+    public RoundRobinTournament(PrisonerPlayer... prisoners) {
         this.prisoners = new ArrayList<>();
         for (final PrisonerPlayer prisoner : prisoners) {
             ScoredPrisoner sp = new ScoredPrisoner(prisoner);
             this.prisoners.add(sp);
+        }
+    }
+
+    public void runTournament() {
+        final int len = prisoners.size();
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                double[] account = new double[]{0, 0};
+
+                ScoredPrisoner ScoredPrisoner1 = prisoners.get(i);
+                ScoredPrisoner ScoredPrisoner2 = prisoners.get(j);
+
+                for (int k = 0; k < GAMES_PER_SET; k++) {
+                    ScoredPrisoner1.getPrisoner().reset();
+                    ScoredPrisoner2.getPrisoner().reset();
+                    TwoPlayerGame game = new TwoPlayerGame(ScoredPrisoner1.getPrisoner(), ScoredPrisoner2.getPrisoner());
+
+                    double[] gameScore = game.play();
+
+                    account[0] += gameScore[0];
+                    account[1] += gameScore[1];
+                }
+
+                System.out.println(
+                        ScoredPrisoner1.getPrisoner().getClass().getName()
+                        + " vs. "
+                        + ScoredPrisoner2.getPrisoner().getClass().getName()
+                        + ": "
+                        + Arrays.toString(account));
+
+                ScoredPrisoner1.addToScore(account[0]);
+                ScoredPrisoner2.addToScore(account[1]);
+            }
+        }
+
+        // now print the results
+        System.out.println("\nRESULTS:");
+        for (ScoredPrisoner sp : prisoners) {
+            System.out.println(sp.getPrisoner().getClass().getName() + " " + sp.getScore());
         }
     }
 
