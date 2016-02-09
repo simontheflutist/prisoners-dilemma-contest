10c9a9e96c0e5cd4afa8ad29800fd6f8b512ff8c
Simon Kuang <simonkuangflutist@gmail.com>
2016-02-09 14:29:09 -0800

    two players as attributes, initialize in constructor

--- src/prisonersdilemmacontest/TwoPlayerGame.java
+++ src/prisonersdilemmacontest/TwoPlayerGame.java
@@ -8,4 +8,11 @@ package prisonersdilemmacontest;
 public final class TwoPlayerGame {
 
     public static final short NUMBER_OF_ROUNDS = 10;
+
+    private final PrisonerPlayer player1, player2;
+
+    public TwoPlayerGame(final PrisonerPlayer player1, final PrisonerPlayer player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 }
