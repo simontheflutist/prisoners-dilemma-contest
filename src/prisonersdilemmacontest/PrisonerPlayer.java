695e2dd8a7f3e2c03c584c82c862955bdc531add
Simon Kuang <simonkuangflutist@gmail.com>
2016-02-09 14:23:11 -0800

    document PrisonerPlayer

--- src/prisonersdilemmacontest/PrisonerPlayer.java
+++ src/prisonersdilemmacontest/PrisonerPlayer.java
@@ -1,18 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prisonersdilemmacontest;
 
 import java.security.SecureRandom;
 import java.util.Random;
 
+/**
+ * Represents a single prisoner playing in our game.
+ *
+ * @author simon
+ */
 public abstract class PrisonerPlayer {
 
+    /**
+     * You will use this Random instance if you need a random boolean/double,
+     * etc.
+     */
     protected static final Random random = new SecureRandom();
 
+    /**
+     * Compute the next move that you want your prisoner to make.
+     *
+     * @return the move that the prisoner submits: 1 is nice and 0 is mean
+     */
     public abstract boolean nextMove();
 
+    /**
+     * Process the opponent's move (e.g. keep a history for stats or for move
+     * copying)
+     *
+     * @param oppMove
+     */
     public abstract void processOpponentMove(boolean oppMove);
 }
