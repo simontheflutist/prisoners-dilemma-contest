164c693ce1e2af5ec3e05169ce53bf54e96e60ad
Simon Kuang <simonkuangflutist@gmail.com>
2016-02-09 16:40:26 -0800

    added Simon's Bayesian Estimator

--- src/prisonersdilemmacontest/PrisonersDilemmaContest.java
+++ src/prisonersdilemmacontest/PrisonersDilemmaContest.java
@@ -5,13 +5,14 @@ import prisonersdilemmacontest.prisoners.CopyMovePrisonerNice;
 import prisonersdilemmacontest.prisoners.MeanPrisoner;
 import prisonersdilemmacontest.prisoners.NicePrisoner;
 import prisonersdilemmacontest.prisoners.RandomPrisoner;
+import prisonersdilemmacontest.prisoners.SimonBayesPrisoner;
 
 public class PrisonersDilemmaContest {
 
     public static void main(String[] args) {
 //        TwoPlayerGame game = new TwoPlayerGame(new RandomPrisoner(), new CopyMovePrisonerMean());
 //        System.out.println(Arrays.toString(game.play()));
-        RoundRobinTournament rrt = new RoundRobinTournament(new RandomPrisoner(), new MeanPrisoner(), new NicePrisoner(), new CopyMovePrisonerMean(), new CopyMovePrisonerNice());
+        RoundRobinTournament rrt = new RoundRobinTournament(new RandomPrisoner(), new MeanPrisoner(), new NicePrisoner(), new CopyMovePrisonerMean(), new CopyMovePrisonerNice(), new SimonBayesPrisoner());
         rrt.runTournament();
     }
 }
---
+++ src/prisonersdilemmacontest/prisoners/SimonBayesPrisoner.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prisonersdilemmacontest.prisoners;
+
+import prisonersdilemmacontest.PrisonerPlayer;
+
+/**
+ * Play stochastically using Simon's curious method, in which you play nice as
+ * often as the opponent plays mean.
+ *
+ * @author simon
+ */
+public class SimonBayesPrisoner extends PrisonerPlayer {
+
+    private int n = 0, m = 0;
+
+    @Override
+    public boolean nextMove() {
+        double u = (m + 1) / (n + m + 2);
+        return random.nextDouble() < u;
+    }
+
+    @Override
+    public void processOpponentMove(boolean oppMove) {
+        if (oppMove) { // if nice
+            n++;
+        } else {
+            m++;
+        }
+    }
+
+}
